13643de6f28222b23db90ae038330256f8c81c89
Niall Coady <niall_coady7@hotmail.com>
2017-10-11 21:45:36 +0100

    EditText and button added to activity_tweet

--- app/src/main/java/coady/mytweetapp/Tweet.java
+++ app/src/main/java/coady/mytweetapp/Tweet.java
@@ -2,12 +2,24 @@ package coady.mytweetapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class Tweet extends AppCompatActivity {
+
+    private Button tweetButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tweet);
+
+        tweetButton = (Button) findViewById(R.id.tweetButton);
+    }
+
+    public void tweetButtonPressed (View view) {
+        Log.v("Tweet", "Tweet Pressed!");
     }
 }
+
