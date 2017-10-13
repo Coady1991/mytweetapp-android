3bc36bea5a7377e4abc3da463b857bb4ce3b03c7
Niall Coady <niall_coady7@hotmail.com>
2017-10-13 13:00:02 +0100

    Added main_menu.xml, which shows a Toast

--- app/src/main/java/coady/mytweetapp/Tweet.java
+++ app/src/main/java/coady/mytweetapp/Tweet.java
@@ -3,8 +3,13 @@ package coady.mytweetapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
 
 public class Tweet extends AppCompatActivity {
 
@@ -20,6 +25,27 @@ public class Tweet extends AppCompatActivity {
 
     public void tweetButtonPressed (View view) {
         Log.v("Tweet", "Tweet Pressed!");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.menuTimeline:
+                Toast.makeText(this, "Timeline Selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menuSettings:
+                Toast.makeText(this, "Settings Selected", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
 
