2f7453b397137b042f354a0f8a4e25d2237c08b5
Niall Coady <niall_coady7@hotmail.com>
2017-10-15 14:38:34 +0100

    Login and SignUp button launch Tweet Activity for now

--- app/src/main/java/coady/mytweetapp/Welcome.java
+++ app/src/main/java/coady/mytweetapp/Welcome.java
@@ -1,14 +1,24 @@
 package coady.mytweetapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
-public class Welcome extends AppCompatActivity{
+public class Welcome extends AppCompatActivity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    public void loginPressed (View view) {
+        startActivity (new Intent(this, Tweet.class));
+    }
+    
+    public void signupPressed(View view) {
+        startActivity(new Intent(this, Tweet.class));
     }
 }
