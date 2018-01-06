8cf6694edbe5415343403b4e6e062067d2afa396
Niall Coady <niall_coady7@hotmail.com>
2018-01-06 12:42:25 +0000

    Added Coords and Marker models

---
+++ app/src/main/java/coady/mytweetapp/model/Coords.java
@@ -0,0 +1,10 @@
+package coady.mytweetapp.model;
+
+/**
+ * Created by Coady on 06/01/2018.
+ */
+
+public class Coords {
+    public double latitude;
+    public double longitude;
+}
---
+++ app/src/main/java/coady/mytweetapp/model/Marker.java
@@ -0,0 +1,11 @@
+package coady.mytweetapp.model;
+
+/**
+ * Created by Coady on 06/01/2018.
+ */
+
+public class Marker {
+    public int id = 1;
+    public Coords coords = new Coords();
+}
+
